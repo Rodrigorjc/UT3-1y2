ebf7e9213e5056710877c0b7cd4593e25c7d3e82
Rodrigo <rjaencobos@safareyes.es>
2024-11-01 10:14:10 +0100

    Changes 01/11/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FechaHoraComponentComponent} from './fecha-hora-component/fecha-hora-component.component';
+import {NumeroAleatorioComponentComponent} from './numero-aleatorio-component/numero-aleatorio-component.component';
+import {HistorialComponent} from './historial/historial.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FechaHoraComponentComponent, NumeroAleatorioComponentComponent, HistorialComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
